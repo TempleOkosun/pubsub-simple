5d4d867ad438e49159a754a2af75c4ddfdc9532c
Temple Okosun <okosun.temple@gmail.com>
2023-08-25 15:31:37 -0400

    simple pub-sub gcp messaging

---
+++ src/main/java/com/example/pubsubsimple/PubsubSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.example.pubsubsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubsubSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubsubSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pubsubsimple/PubsubSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pubsubsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubsubSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

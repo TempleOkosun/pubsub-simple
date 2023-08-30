c41678390d10fcf79044b022f770e8019aca99ee
Temple Okosun <okosun.temple@gmail.com>
2023-08-30 15:50:04 -0400

    added hello5

---
+++ src/main/java/com/example/pubsubsimple/Hello5.java
@@ -0,0 +1,26 @@
+package com.example.pubsubsimple;
+
+public class Hello5 {
+    private String message;
+    private String address;
+
+    public Hello5(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}

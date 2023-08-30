518c9d92ab2d11b794b54b9c08f869ddf847a580
Temple Okosun <okosun.temple@gmail.com>
2023-08-30 15:43:43 -0400

    added address to hello3

--- src/main/java/com/example/pubsubsimple/Hello3.java
+++ src/main/java/com/example/pubsubsimple/Hello3.java
@@ -2,6 +2,7 @@ package com.example.pubsubsimple;
 
 public class Hello3 {
     private String message;
+    private String address;
 
     public Hello3(String message) {
         this.message = message;
@@ -13,5 +14,13 @@ public class Hello3 {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }

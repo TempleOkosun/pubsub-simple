06b5fb071fee726764eb511a09f11e6e394e4a6f
Temple Okosun <okosun.temple@gmail.com>
2023-08-31 04:38:33 -0400

    added hello7

---
+++ src/main/java/com/example/pubsubsimple/Hello7.java
@@ -0,0 +1,17 @@
+package com.example.pubsubsimple;
+
+public class Hello7 {
+    private String message;
+
+    public Hello7(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
